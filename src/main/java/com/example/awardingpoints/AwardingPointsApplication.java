1c329d06ca56240b1263768dc3418988ce73bac5
taniusa90 <galea_tanea@mail.com>
2023-02-27 15:33:29 -0600

    Initial commit

---
+++ src/main/java/com/example/awardingpoints/AwardingPointsApplication.java
@@ -0,0 +1,13 @@
+package com.example.awardingpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwardingPointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwardingPointsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awardingpoints/AwardingPointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awardingpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwardingPointsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
